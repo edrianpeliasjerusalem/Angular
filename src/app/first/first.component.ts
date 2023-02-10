ac52c9c4d8f3880ac9b10faa1649d6d337aabf7d
edrianpeliasjerusalem <edrianpeliasjerusalem@gmail.com>
2023-02-11 03:47:08 +0800

    add Class and Syle bindings

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -9,7 +9,19 @@ export class FirstComponent implements OnInit {
 
   public id = "lblName"
   public isDisabled = true;
-
+  public successClass = "text-success";
+  public isError = true;
+  public isItalic = true;
+  public errorMsg = {
+    "text-danger": this.isError,
+    "text-success": !this.isError,
+    "text-italic": this.isItalic
+  }
+  // cannot use hypen, use CamelCase
+  public titleStyle = {
+    color: "orange",
+    fontStyle: "italic"
+  }
   constructor() { }
 
   ngOnInit() {
